17913d8caa2bde20c872315a68be66d207d80ecf
Rustem Mussabekov <winlinx@ya.ru>
2020-12-15 16:08:17 +0300

    - On the fly update of bookmarks and collections - Upgrade dependencies

--- src/contextMenu.js
+++ src/contextMenu.js
@@ -1,11 +1,12 @@
-const { app } = require('electron')
+const { app, remote } = require('electron')
 const isDev = require('electron-is-dev')
 const contextMenu = require('electron-context-menu')
 
 module.exports = function() {
-	app.on('web-contents-created', (e, window) => {
+	app.on('web-contents-created', (e, win) => {
         contextMenu({
-            window,
+            //instead of just window: win there is fix for electron 11
+            window: win.getType && win.getType() == 'webview' ? win : win.webContents || (win.getWebContentsId && remote.webContents.fromId(win.getWebContentsId())),
             showCopyImageAddress: true,
             showSaveImageAs: true,
             showServices: true,
