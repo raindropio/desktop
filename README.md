99870b59449cf8e77e4ad755604a5785eadd9b61
Rustem Mussabekov <winlinx@ya.ru>
2021-04-01 11:00:24 +0300

    Turn off update lib for Windows Store version

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Then check `out` folder
 
 # Deployment
 Run `yarn deploy:prod`
+
+# Windows Appx Specific Tips
+- Install unsigned copy
+https://stackoverflow.com/questions/23812471/installing-appx-without-trusted-certificate
--- src/update.js
+++ src/update.js
@@ -3,6 +3,9 @@ const isSquirrelStartup = require('electron-squirrel-startup')
 const updateApp = require('update-electron-app')
 
 module.exports = function() {
+    if (process.windowsStore)
+        return
+
     if (isSquirrelStartup)
         return app.quit()
 
